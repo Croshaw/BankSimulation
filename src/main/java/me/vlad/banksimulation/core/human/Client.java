74b0d7bbd1ce78326041bc9a655e28fbbab10bdc
Croshaw <crosafixx200215@gmail.com>
2023-12-19 00:02:42 +0500

    feat: added Client class

---
+++ src/main/java/me/vlad/banksimulation/core/human/Client.java
@@ -0,0 +1,7 @@
+package me.vlad.banksimulation.core.human;
+
+public class Client extends People {
+    public Client(String surname, String name, String patronymic) {
+        super(surname, name, patronymic);
+    }
+}
