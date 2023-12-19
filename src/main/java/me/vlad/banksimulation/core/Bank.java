1d25f7dd5e7bdc16a29871e343b94a38a49caf30
Croshaw <crosafixx200215@gmail.com>
2023-12-19 12:01:09 +0500

    feat: added to Bank a little bit logic and some informative methods

--- src/main/java/me/vlad/banksimulation/core/Bank.java
+++ src/main/java/me/vlad/banksimulation/core/Bank.java
@@ -18,6 +18,7 @@ public class Bank {
     private final int maxQueueLength;
     private final int maxClerksCount;
     private final InfoBoard infoBoard;
+    private int number;
     public Bank(LocalDateTime dateTimeOfCreation, HashMap<DayOfWeek, Pair<LocalTime, Duration>> schedule, int maxQueueLength, int maxClerksCount) {
         this.dateTimeOfCreation = dateTimeOfCreation;
         this.schedule = schedule;
@@ -27,13 +28,20 @@ public class Bank {
         maxAndMinQueueLength = new HashMap<>();
         maxAndMinQueueLength.put(dateTimeOfCreation.toLocalDate(), new Pair<>(Integer.MAX_VALUE, Integer.MIN_VALUE));
         reputation = 1;
+        number = 1;
         infoBoard = new InfoBoard();
     }
     public boolean hire(Clerk clerk) {
         return clerks.add(clerk);
     }
-    public void registerRequest(Request request) {
+    public boolean registerRequest(Request request) {
+        if(queue.size() == maxQueueLength) {
+            reputation = Math.max(reputation-0.05, 0);
+            return false;
+        }
         queue.offer(request);
+        number++;
+        return true;
     }
     private long workByClerk(long secondsStep, Clerk clerk) {
         return clerk.work(secondsStep, this);
@@ -73,21 +81,112 @@ public class Bank {
             var tempSchedule = schedule.get(prevDateTime.getDayOfWeek());
             long dif = Math.abs(Duration.between(tempSchedule.getFirst().plus(tempSchedule.getSecond()), prevDateTime).toSeconds());
             work(dif);
+            number = 1;
         } else if(!isWork(prevDateTime) && isWork(curDateTime)) {
             var tempSchedule = schedule.get(prevDateTime.getDayOfWeek());
             long dif = Math.abs(Duration.between(tempSchedule.getFirst(), prevDateTime).toSeconds());
             work(dif);
+        } else {
+            if(number != 1)
+                number = 1;
         }
     }
-
     public boolean isWork() {
         return isWork(getCurrentDateTime());
     }
     public boolean isWork(LocalDateTime time) {
+        if(!schedule.containsKey(time.getDayOfWeek()))
+            return false;
         var tempSchedule = schedule.get(time.getDayOfWeek());
         return time.toLocalTime().isAfter(tempSchedule.getFirst()) && time.toLocalTime().isBefore(tempSchedule.getFirst().plus(tempSchedule.getSecond()));
     }
     public LocalDateTime getCurrentDateTime() {
         return dateTimeOfCreation.plus(currentDuration);
     }
+    public int getNumberRequest() {
+        return number;
+    }
+    public double getReputation() {
+        return reputation;
+    }
+    public int getMinQueueLengthByDate(LocalDate date) {
+        if(!maxAndMinQueueLength.containsKey(date))
+            return 0;
+        return maxAndMinQueueLength.get(date).getFirst();
+    }
+    public int getMinQueueLength() {
+        int min = Integer.MAX_VALUE;
+        for(var date : maxAndMinQueueLength.keySet()) {
+            min = Math.min(min, getMinQueueLengthByDate(date));
+        }
+        return min;
+    }
+    public int getMaxQueueLengthByDate(LocalDate date) {
+        if(!maxAndMinQueueLength.containsKey(date))
+            return 0;
+        return maxAndMinQueueLength.get(date).getSecond();
+    }
+    public int getMaxQueueLength() {
+        int max = Integer.MIN_VALUE;
+        for(var date : maxAndMinQueueLength.keySet()) {
+            max = Math.max(max, getMaxQueueLengthByDate(date));
+        }
+        return max;
+    }
+    public int getAverageQueueLengthByDate(LocalDate date) {
+        if(!maxAndMinQueueLength.containsKey(date))
+            return 0;
+        var pair = maxAndMinQueueLength.get(date);
+        return (pair.getFirst() + pair.getSecond()) / 2;
+    }
+    public int getAverageQueueLength() {
+        if(maxAndMinQueueLength.isEmpty())
+            return 0;
+        int avg = 0;
+        for(var date : maxAndMinQueueLength.keySet()) {
+            avg += getAverageQueueLengthByDate(date);
+        }
+        return avg / maxAndMinQueueLength.size();
+    }
+    public int getNumberOfServedClientsByClerkAndDate(Clerk clerk, LocalDate date) {
+        return clerk.getNumberOfServedClientsByDate(date);
+    }
+    public int getNumberOfServedClientsByDate(LocalDate date) {
+        int number = 0;
+        for(var clerk : clerks) {
+            number += clerk.getNumberOfServedClientsByDate(date);
+        }
+        return number;
+    }
+    public int getNumberOfServedClientsByClerk(Clerk clerk) {
+        return clerk.getNumberOfServedClients();
+    }
+    public int getTotalNumberOfServedClients() {
+        int number = 0;
+        for(var clerk : clerks) {
+            number += clerk.getNumberOfServedClients();
+        }
+        return number;
+    }
+    public int getNumberOfLostClientsByClerkAndDate(Clerk clerk, LocalDate date) {
+        return clerk.getNumberOfLostClientsByDate(date);
+    }
+    public int getNumberOfLostClientsByDate(LocalDate date) {
+        int number = 0;
+        for(var clerk : clerks) {
+            number += clerk.getNumberOfLostClientsByDate(date);
+        }
+        return number;
+    }
+    public int getNumberOfLostClientsByClerk(Clerk clerk) {
+        return clerk.getNumberOfLostClients();
+    }
+    public int getTotalNumberOfLostClients() {
+        int number = 0;
+        for(var clerk : clerks) {
+            number += clerk.getNumberOfLostClients();
+        }
+        return number;
+    }
+
 }
