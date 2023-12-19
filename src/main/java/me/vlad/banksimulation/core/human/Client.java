f35af68941d7ea317b71835aedd8e48da2cbe9e2
Croshaw <crosafixx200215@gmail.com>
2023-12-19 11:59:45 +0500

    feat: added to Client comeToBank method

--- src/main/java/me/vlad/banksimulation/core/human/Client.java
+++ src/main/java/me/vlad/banksimulation/core/human/Client.java
@@ -1,7 +1,17 @@
 package me.vlad.banksimulation.core.human;
 
+import me.vlad.banksimulation.core.Bank;
+import me.vlad.banksimulation.core.Request;
+
+import java.time.Duration;
+
 public class Client extends People {
     public Client(String surname, String name, String patronymic) {
         super(surname, name, patronymic);
+    }
+
+    public void comeToBank(Bank bank, Duration serviceDuration, double price) {
+        Request request = new Request(bank.getNumberRequest(), price, this, serviceDuration, bank.getCurrentDateTime());
+        bank.registerRequest(request);
     }
 }
